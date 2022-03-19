08dac58eb94117a545ea1fe36435fcc43d46e1d9
Javier Juan Oltra <javijuol@gmail.com>
2022-03-19 14:09:41 +0100

    Update status.go

--- infra/git/status.go
+++ infra/git/status.go
@@ -15,7 +15,7 @@ func Status(path string) (output string, err error) {
 	if err != nil {
 		return
 	}
-	commandOutput, err := exec.Command("git", "status", "-sbuno").Output()
+	commandOutput, err := exec.Command("git", "status", "-sbuno", "--ignore-submodules").Output()
 	if err != nil {
 		return
 	}
